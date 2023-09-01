1dccf4173919793b33a101c5aa83f2d361ae8c41
ShiSian <1126580806@qq.com>
2023-09-01 19:39:00 +0800

    TmpVersion -- 20230901

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Xml.Linq;
 
 namespace TestProject
@@ -27,6 +28,42 @@ namespace TestProject
 
 
             Console.ReadLine();
+        }
+
+        void WriteXML()
+        {
+            // 1、创建Dom对象
+            XDocument xDoc = new XDocument();
+            XDeclaration xDec = new XDeclaration("1.0", "utf-8", null);
+            xDoc.Declaration = xDec;
+
+            //2.创建根节点
+            XElement rootElement = new XElement ("List"); 
+            xDoc.Add (rootElement);
+
+            //3.循环工ist集合创建Person节点
+            for (int i = 0; i < 4; i++)
+            { 
+                //为每个Person对象创建一个Person元素
+                XElement xElementPerson = new XElement("Person");
+                xElementPerson.SetAttributeValue("id", (i + 1).ToString());
+                xElementPerson.SetElementValue("Name", "Name"+ (i + 1).ToString());
+                rootElement.Add(xElementPerson);
+            }
+
+            //4.保存到文件
+            xDoc.Save("ListNex.xml");
+        }
+
+        void ReadXml()
+        {
+            //1.读取xm1文件(XDocument)
+            //1.加载xm1文件
+            XDocument document = XDocument.Load("rss sportslg.xml");
+            //2,先获取根节点
+            XElement rootElement = document.Root;
+            //3.将xml的根元素加载到Treeview的根节点上
+            TreeNode rootNode = treeview1.Nodes.Add(rootElement.Name.Tostring())
         }
     }
 }
